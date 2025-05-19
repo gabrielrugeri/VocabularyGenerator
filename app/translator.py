b7fdb0680894e199881fb3e4f8ae78520c7836aa
Gabriel Rugeri <gabriel.rugeri@outlook.com>
2025-05-19 10:08:45 -0300

    included DeepL API key

--- app/translator.py
+++ app/translator.py
@@ -2,7 +2,7 @@ import requests
 
 def translate_with_deepl(text, source_lang="EN", target_lang="PT"):
     url = "https://api-free.deepl.com/v2/translate"
-    headers = {"Authorization": "DeepL-Auth-Key YOUR_API_KEY"}
+    headers = {"Authorization": "DeepL-Auth-Key 8a253586-1b4d-4077-abca-10475839ecd5:fx"}
     data = {
         "text": text,
         "source_lang": source_lang,
